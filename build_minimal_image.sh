fb01d8a674214d610eaa3f34b939ae74d8ad4499
matthewcmead <matthewcmead@gmail.com>
2017-08-07 15:56:49 -0400

    leave app.tar.gz around for offline use if necessary

--- build_minimal_image.sh
+++ build_minimal_image.sh
@@ -3,4 +3,3 @@
 docker build -t matthewcmead/tileserver-gl-centos7-big .
 docker run -i --rm --entrypoint /bin/bash matthewcmead/tileserver-gl-centos7-big -c "cd /usr/src && tar cf - app" |gzip -c >app.tar.gz
 docker build -f Dockerfile_from_app_tar -t matthewcmead/tileserver-gl-centos7 .
-rm -f app.tar.gz
